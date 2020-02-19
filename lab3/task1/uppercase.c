d43df25f29fe51d7df069cfe1a6c3a6b0e5fcbd9
gitteej <tj.d.carlson@gmail.com>
2020-02-19 12:06:35 -0500

    Task1 complete

--- lab3/task1/uppercase.c
+++ lab3/task1/uppercase.c
@@ -17,6 +17,19 @@ int main(int argc, char *argv[]) {
    return 0;
 
  */
+	char str[10090];
+	int ch, n = 0;
+
+	while ((ch = getchar()) != EOF && n < 1000) {
+		//97-122 == lowercase char
+		str[n] = ch;
+		n++;
+		//putchar(str[n]);
+		if(ch > 96 && ch < 123) {
+			ch -= 32;
+		}
+		printf("\n%c\n",ch);
+	}
 
     return 0;
 }
