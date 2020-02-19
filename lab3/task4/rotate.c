6cd9111f1a9adc7030320a4ef431d70be5c5aa73
gitteej <tj.d.carlson@gmail.com>
2020-02-19 14:15:54 -0500

    Task4 complete.

--- lab3/task4/rotate.c
+++ lab3/task4/rotate.c
@@ -2,10 +2,19 @@
 #include <stdlib.h>
 
 void rotate_4(int *p_a, int*p_b, int*p_c, int* p_d) {
+    int swap = *p_a;
+    *p_a = *p_b;
+    *p_b = *p_c;
+    *p_c = *p_d;
+    *p_d = swap;
     //TO-DO: please implement left rotate 4 integers by one position
 }
 
 int main(int argc, char ** argv) {
+    if(argc != 5){
+        printf("ERROR: 4 numbers required");
+        return 0;
+    }
     //TO-DO check if there are 4 inputs.
     //print out an error message if there are not enough inputs.
     //hint: use argc
