94a376fe2fb55fa43d244e0bf373ba63f029a364
gitteej <tj.d.carlson@gmail.com>
2020-02-25 12:40:26 -0500

    task 7 complete

--- lab3/task7/bubble.c
+++ lab3/task7/bubble.c
@@ -3,49 +3,73 @@
 #include <stdio.h>  /* Need for standard I/O functions */
 #include <string.h> /* Need for strlen() */
 
+//#define NUM 30   /* number of strings */
+#define NUM 5
 
-#define NUM 30   /* number of strings */
 #define LEN 1200  /* max length of each string */
+
+void swap(char a[], char b[]){
+
+  char temp_str[strlen(a)];
+
+  for(int i = 0; i < strlen(a); i++){
+    temp_str[i] = a[i];
+  }
+
+  for(int i = 0; i <= strlen(b); i++){
+
+    if(i == strlen(b)){
+      a[i] = '\0';
+    } else {
+      a[i] = b[i];     
+    }
+  }
+  for(int i = 0; i <= strlen(temp_str); i++){
+    if(i == strlen(temp_str)){
+      b[i] = '\0';
+    } else {
+      b[i] = temp_str[i];
+    } 
+  }
+}
 
 int main()
 {
   char Strings[NUM][LEN];
 
-  printf("Please enter %d strings, one per line:\n", NUM);
-
-  /* Write a for loop here to read NUM strings.
-
-     Use fgets(), with LEN as an argument to ensure that an input line that is too
-     long does not exceed the bounds imposed by the string's length.  Note that the
-     newline and NULL characters will be included in LEN.
-  */
+  for(int k = 0; k < NUM; k++){     
+    fgets(Strings[k],LEN,stdin);
+  }
 
   puts("\nHere are the strings in the order you entered:");
+  for(int i = 0; i < NUM; i++){
+    for(int l = 0; l < strlen(Strings[i]); l++){
+      printf("%c",Strings[i][l]);
+    }
+  }
 
-  /* Write a for loop here to print all the strings. */
+  int sorted = 0;
 
-  
-  /* Bubble sort */
-  /* Write code here to bubble sort the strings in ascending alphabetical order
-
-     Your code must meet the following requirements:
-        (i) The comparison of two strings must be done by checking them one
-            character at a time, without using any C string library functions.
-            That is, write your own while/for loop to do this.
-       (ii) The swap of two strings must be done by copying them 
-            (using a temp variable of your choice) one character at a time,
-            without using any C string library functions.
-            That is, write your own while/for loop to do this.
-      (iii) You are allowed to use strlen() to calculate string lengths.
-  */
-
-  
-  
-  /* Output sorted list */
+  while(!sorted){
+    sorted = 1; 
+    for(int i = 0; i < NUM-1; i++){
+      for(int j = 0; j < strlen(Strings[i]) && strlen(Strings[i+1]) != j; j++){
+        if(Strings[i][j] < Strings[i+1][j]){
+          break;
+        } else if(Strings[i][j] > Strings[i+1][j]){
+          swap(Strings[i],Strings[i+1]);
+          sorted = 0;
+          break;
+        }
+      }
+    }
+  }
   
   puts("\nIn alphabetical order, the strings are:");     
-  /* Write a for loop here to print all the strings. Feel free to use puts/printf
-     etc. for printing each string.
-  */
+  for(int i = 0; i < NUM; i++){
+
+    printf("%s",Strings[i]);
+  }
+
 
 }
