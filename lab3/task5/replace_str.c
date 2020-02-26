a25a42ce7c8f744af4e9fd668ba15e49fafa8474
gitteej <tj.d.carlson@gmail.com>
2020-02-26 16:14:23 -0500

    Task5 partially complete. Cannot solve.

--- lab3/task5/replace_str.c
+++ lab3/task5/replace_str.c
@@ -1,62 +1,37 @@
-#include <stdio.h>
 /* Overwrites an inputted string with 232 is awesome! if there is room.
  * * Does nothing if there is not. */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /*
-Questions...
-This code doesn't actually take/pass any arguments. 
-Example in lab document doesn't show any parameters passed...
+Overwrites an inputted string with 232 is awesome! if there is room.
+ * * Does nothing if there is not. 
 */
 
 void CS232(char* src) {
-     char *srcptr, replaceptr; 
+     char *srcptr, *replaceptr; 
      // char *srcptr, *replaceptr;
-     int srcLength = sizeof(src);
+     int srcLength = sizeof(*src);
+     printf("srcLength: %s",src);
      char replacement[15] = "232 is awesome!";
      srcptr = src;
-     replaceptr = *replacement;
+     replaceptr = replacement;
      if (srcLength >= 15) {
-         //for (int i = 0; i < 15; i++){
-            *srcptr = replaceptr;
-            //src[i] = replacement[i];
-         //}
-            
+         for (int i = 0; i < 15; i++)
+          *srcptr++ = *replaceptr++;
      }
 }
 
-char * replace_string(char* text) {
-    CS232(text);
-    return text;
-    //
-
-}
-
-int main(int argc, char ** argv) {
-    //DO I NEED TO ADD PASSED PARAMETER BELOW??
-    char * ret = replace_string(argv[1]);
-    //
-    printf("replaced string = %s\n", ret);
-
-    return 0;
-}
-
-/*
-char * replace_string(char* text) {
-    char str[15];
+char * replace_string() {
+    //char str[15];
+    char* str = (char*)calloc(15,sizeof(char));
     CS232(str);
-    //problem with this line
-    //str is local variable. can't return this.
     return str;
-    //
 
 }
 
 int main(int argc, char ** argv) {
-    //DO I NEED TO ADD PASSED PARAMETER BELOW??
-    char * ret = replace_string(argv[1]);
-    //
+    char * ret = replace_string();
     printf("replaced string = %s\n", ret);
-
     return 0;
 }
-*/
