1f437eacb69b4de21385284e49e9a71e8f210c77
gitteej <tj.d.carlson@gmail.com>
2020-03-09 02:54:34 -0400

    Copy of task4 files to task5

---
+++ lab5/task5/snode.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "snode.h"
+#include <string.h>
+#include <stdlib.h>
+
+struct snode *snode_create(char *s) 
+{
+	struct snode *node_t = (struct snode*)malloc(sizeof(struct snode));
+	node_t->str = (char*)malloc(strlen(s)*sizeof(char)+1);
+	strcpy(node_t->str,s);
+	node_t->next = NULL;
+
+ //TODO: implement snode_create, change the prototype to
+ //match with header file
+ //return node;
+	return node_t;
+
+  
+}
+void snode_destroy(struct snode * s) 
+{
+	free(s->str);
+	free(s);
+
+};
