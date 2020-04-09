e106fb8b0f5acf86787d21b1f8e2ce87a8c1aec4
gitteej <tj.d.carlson@gmail.com>
2020-04-08 23:12:58 -0400

    Part4 question and reverse engineering completed.

--- lab7/task4/main.c
+++ lab7/task4/main.c
@@ -1,8 +1,10 @@
 int main() {
     int a=13,b=5, c=0;
     if(a >6) {
-        c += 2;
-    } else {
-        c -= 2;
-    }
+        b=15;
+    } 
+    while(15 > c){
+        a += c;
+        c++;
+    } 
 }
