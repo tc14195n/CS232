dffae22a24ad240a82d89147d15a538d7d1b98cb
gitteej <tj.d.carlson@gmail.com>
2020-04-09 10:19:25 -0400

    Task5 updates. not complete.

--- lab7/task5/main.c
+++ lab7/task5/main.c
@@ -1,13 +1,26 @@
 char password[8] = "secret";
+int i = 0;
 int main() {
     char input[8];
+    int i;
     printf("please enter your password\n");
     scanf("%s", input);
+    
+    
+    for(i = 0; i < 8; i++){
+        if(64 < (i-8)){
 
+        } else if(64 > (i-8)){
+            
+        }
+    }
+    
+
+    
+    
     if(strcmp(input, password)==0) {
         return 0;
     } else {
         return -1;
     }
-
 }
