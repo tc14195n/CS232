9d670977f5fc2baca772c51dc2c869b9bd90ef1a
tc14195n <tc14195n@pace.edu>
2020-05-09 10:33:29 -0400

    Task1
    Code implemented and some questions answered

--- lab10/task1/matrix_mul_jki.c
+++ lab10/task1/matrix_mul_jki.c
@@ -9,7 +9,7 @@ int C[Dim][Dim];
 int
 main()
 {
-    int i, j, k;
+    int i, j, k, r;
 
     for (i = 0; i < Dim; i++)		/* first initialize the matrices */
 	for (j = 0; j < Dim; j++) {
@@ -18,10 +18,13 @@ main()
 	     C[i][j] = 0;
 	}
 
-    for (i = 0; i < Dim; i++)		/* then multiply them together */
-	for (j = 0; j < Dim; j++)
-            for (k = 0; k < Dim; k++)
-		 C[i][j] += A[i][k] * B[k][j];
+    for(j = 0; j < Dim; j++){
+    	for(k = 0;k < Dim; k++){
+    		r = B[k][j];
+    		for(i = 0; i < Dim; i++)
+    			C[i][j] += A[i][k] * r;
+    	}
+    }
 
     printf("C[%d][%d] = %d\n", Dim-1, Dim-1, C[Dim-1][Dim-1]);
     return (C[Dim-1][Dim-1]);		/* and then we're done */
